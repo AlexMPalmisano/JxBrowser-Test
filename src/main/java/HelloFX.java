14a13b5b3e120851591976774adf8144392900f7
AlexMPalmisano <AlexMPalmisano@gmail.com>
2023-05-08 16:02:14 -0400

    updating pom file

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -12,37 +12,37 @@ import javafx.stage.Stage;
 
 public class HelloFX extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+//    @Override
+//    public void start(Stage stage) {
+//        String javaVersion = System.getProperty("java.version");
+//        String javafxVersion = System.getProperty("javafx.version");
+//        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+//        Scene scene = new Scene(new StackPane(l), 640, 480);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
 
-//     @Override
-//     public void start(Stage primaryStage) {
-//         // Initialize Chromium.
-//         Engine engine = Engine.newInstance(HARDWARE_ACCELERATED);
-//
-//         Browser browser = engine.newBrowser();
-//
-//         // Load the required web page.
-//         browser.navigation().loadUrl("https://html5test.com");
-//
-//         // Create and embed JavaFX BrowserView component to display web content.
-//         BrowserView view = BrowserView.newInstance(browser);
-//
-//         Scene scene = new Scene(new StackPane(view), 1280, 800);
-//         primaryStage.setTitle("JxBrowser JavaFX");
-//         primaryStage.setScene(scene);
-//         primaryStage.show();
-//
-//         // Shutdown Chromium and release allocated resources.
-//         //primaryStage.setOnCloseRequest(event -> engine.close());
-//     }
+     @Override
+     public void start(Stage primaryStage) {
+         // Initialize Chromium.
+         Engine engine = Engine.newInstance(HARDWARE_ACCELERATED);
+
+         Browser browser = engine.newBrowser();
+
+         // Load the required web page.
+         browser.navigation().loadUrl("https://html5test.com");
+
+         // Create and embed JavaFX BrowserView component to display web content.
+         BrowserView view = BrowserView.newInstance(browser);
+
+         Scene scene = new Scene(new StackPane(view), 1280, 800);
+         primaryStage.setTitle("JxBrowser JavaFX");
+         primaryStage.setScene(scene);
+         primaryStage.show();
+
+         // Shutdown Chromium and release allocated resources.
+         primaryStage.setOnCloseRequest(event -> engine.close());
+     }
 
     public static void main(String[] args) {
         launch();
